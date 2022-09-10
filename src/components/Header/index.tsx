3f79ff05b14daf8593d56d8281a1e30c2818c85c
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-09-10 10:57:59 -0300

    feat: app-theme-switcher

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+import Switch from "react-switch";
+import { ThemeContext } from "styled-components";
+import { shade } from "polished";
+
+import { Container } from "./styles";
+
+interface HeaderProps {
+  toggleTheme(): void;
+}
+
+const Header: React.FC<HeaderProps> = ({ toggleTheme }) => {
+  const { colors, title } = useContext(ThemeContext);
+
+  return (
+    <Container>
+      Theme Switcher
+      <Switch
+        onChange={toggleTheme}
+        checked={title === 'dark'}
+        checkedIcon={false}
+        uncheckedIcon={false}
+        height={10}
+        width={40}
+        handleDiameter={20}
+        offColor={shade(0.5, colors.primary)}
+        onColor={colors.secundary}
+      />
+    </Container>
+  );
+};
+
+export default Header;
---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export default {
+  title: "dark",
+
+  colors: {
+    primary: "#333",
+    secundary: "#091540",
+
+    background: "#222",
+    text: "#fff",
+  },
+};
